27721f1cda1fdb9a93538ecfec78073c4240529d
Edima <edimaudo@gmail.com>
2024-12-21 20:14:36 -0500

    Update header.php

--- web_projects/idea_manager/templates/header.php
+++ web_projects/idea_manager/templates/header.php
@@ -10,6 +10,7 @@
  
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   
     <!-- our custom CSS -->
     <link rel="stylesheet" href="libs/css/custom.css" />
