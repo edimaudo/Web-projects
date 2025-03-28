d27d5615dc569e8a757978fec0d90b8188b78ef6
Edima <edimaudo@gmail.com>
2025-03-28 14:38:11 -0400

    Update course.php

--- fullstack_projects/contosoUniversity/course.php
+++ fullstack_projects/contosoUniversity/course.php
@@ -7,44 +7,76 @@ include('header.php');
 <main role="main" class="pb-3">
 <h2>Courses</h2>
 
-<p> <a href="">Create New</a> </p> 
-<table class="table"> 
-	<thead> 
-		<tr> 
-			<th> Number </th>
-			<th> Title </th> 
-			<th> Department </th> 
-			<th></th> 
-		</tr> 
-	</thead> 
-	<tbody>
-        <!----
-		{% if courses %}
-	 		{% for course in courses %}
-			 <tr> 
-			 	<td> {{ course.id }} </td> 
-			 	<td> {{ course.course_title }} </td> 
-			 	<td> {{ course.course_department_name }} </td> 
-			 	<td>  </td> 
-			 	<td> 
-					<a href="">Edit</a> | 
-					<a href="">Details</a> | 
-					<a href="">Delete</a> 
-				</td>
-			</tr> 
-			</tbody>
-			{% endfor %}
-            !---->
-		</table> 
-		</main> 
-		</div> 
-       <!---- 
-     {% else %}
-     	<h2>No Courses currently in the system</h2>
-     {% endif %}
-     !---->
-</html>
+<p> <a href="course_create.php">Create New</a> </p> 
+<form action="" method="post">    <p>
+Select Department: <select id="SelectedDepartment" name="SelectedDepartment">
+<option value="">All</option>
+<option value="4">Economics</option>
+<option value="3">Engineering</option>
+<option value="1">English</option>
+<option value="2">Mathematics</option>
+</select>
+<input type="submit" value="Filter">
+    </p>
+<input name="_" type="hidden" value="">
+</form>
+
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$sql = "SELECT * FROM Course";
+if($result = $pdo->query($sql)){
+    if($result->rowCount() > 0){
+        echo '<table class="table"> ';
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th> Number </th>";
+        echo "<th> Title </th>";
+        echo "<th> Credit </th>";
+        echo "<th> Department </th>";
+        echo "<th></th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        while($row = $result->fetch()){
+                                    echo "<tr>";
+                                        
+                                        echo "<td>" . $row['CourseID'] . "</td>";
+                                        echo "<td>" . $row['Title'] . "</td>";
+                                        echo "<td>" . $row['Credit'] . "</td>";
+                                        echo "<td>" . $row['DepartmentID'] . "</td>";
+                                        echo "<td>";
+                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="Details" data-toggle="tooltip">|';
+                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Edit" data-toggle="tooltip"></a>|';
+                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Delete" data-toggle="tooltip"></a>';
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            unset($result);
+                        } else{
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else{
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+                    
+                    // Close connection
+                    unset($pdo);
+                
+?>
+
+</main> 
+</div>
+
+	
+
 
 <?php
 include('footer.php');
 ?>
+
+      
+</html>
