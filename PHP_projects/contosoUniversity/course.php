2adfdfd205c45997c405ce559d7a9ed19e8292b2
Edima <edimaudo@gmail.com>
2025-03-31 15:15:22 -0400

    Update course.php

--- PHP_projects/contosoUniversity/course.php
+++ PHP_projects/contosoUniversity/course.php
@@ -8,6 +8,7 @@ include('header.php');
 <h2>Courses</h2>
 
 <p> <a href="course_create.php">Create New</a> </p> 
+
 <form action="" method="post">    <p>
 Select Department: <select id="SelectedDepartment" name="SelectedDepartment">
 <option value="">All</option>
@@ -24,6 +25,18 @@ Select Department: <select id="SelectedDepartment" name="SelectedDepartment">
 <?php
 include 'functions.php';
 $pdo = pdo_connect_mysql();
+
+
+function getDepartmentName($pdo, $departmentId) {
+    $sql = "SELECT Name FROM Department WHERE DepartmentID = :departmentId";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':departmentId', $departmentId, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    $department = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $department ? $department['Name'] : null;
+}
+
 $sql = "SELECT * FROM Course";
 if($result = $pdo->query($sql)){
     if($result->rowCount() > 0){
@@ -32,7 +45,7 @@ if($result = $pdo->query($sql)){
         echo "<tr>";
         echo "<th> Number </th>";
         echo "<th> Title </th>";
-        echo "<th> Credit </th>";
+        echo "<th> Credits </th>";
         echo "<th> Department </th>";
         echo "<th></th>";
         echo "</tr>";
@@ -44,7 +57,7 @@ if($result = $pdo->query($sql)){
                                         echo "<td>" . $row['CourseID'] . "</td>";
                                         echo "<td>" . $row['Title'] . "</td>";
                                         echo "<td>" . $row['Credit'] . "</td>";
-                                        echo "<td>" . $row['DepartmentID'] . "</td>";
+                                        echo "<td>" . getDepartmentName(,$pdo,$row['DepartmentID']) . "</td>";
                                         echo "<td>";
                                             echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="Details" data-toggle="tooltip">|';
                                             echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Edit" data-toggle="tooltip"></a>|';
@@ -67,7 +80,6 @@ if($result = $pdo->query($sql)){
                     unset($pdo);
                 
 ?>
-
 </main> 
 </div>
 
