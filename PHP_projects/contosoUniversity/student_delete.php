16fb7881f11567bfdaf01703454e77742fe51411
Edima <edimaudo@gmail.com>
2025-04-01 15:32:12 -0400

    Update student_delete.php

--- PHP_projects/contosoUniversity/student_delete.php
+++ PHP_projects/contosoUniversity/student_delete.php
@@ -2,47 +2,83 @@
 include('header.php');
 ?>
 
-<div class="container">
-        <main role="main" class="pb-3">
-            
-
-<h1>Delete Student</h1>
-<p class="text-danger"></p>
-<h3>Are you sure you want to delete this?</h3>
-<div>
+<?php
+// Process delete operation after confirmation
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Include config file
+    include 'functions.php';
+    $pdo = pdo_connect_mysql();
     
-    <hr>
-    <dl class="row">
-        <dt class="col-sm-2">
-            Last Name
-        </dt>
-        <dd class="col-sm-10">
-            {{student.student_last_name}}
-        </dd>
-        <dt class="col-sm-2">
-            First Name
-        </dt>
-        <dd class="col-sm-10">
-            {{student.student_first_name}}
-        </dd>
-        <dt class="col-sm-2">
-            Enrollment Date
-        </dt>
-        <dd class="col-sm-10">
-            {{student.student_enrollment_date}}
-        </dd>
-    </dl>
+    // Prepare a delete statement
+    $sql = "DELETE FROM Student WHERE id = :id";
     
-    <form action="" method="post">
-        {% csrf_token %}
-        <input type="hidden" data-val="true" data-val-required="The ID field is required." id="ID" name="ID" value="{{student.id}}">
-        <input type="submit" value="Delete" class="btn btn-danger"> |
-        <a href="{% url 'students' %}">Back to List</a>
-   </form>
-</div>
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_POST["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            // Records deleted successfully. Redirect to landing page
+            header("location: student.php");
+            exit();
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    unset($stmt);
+    
+    // Close connection
+    unset($pdo);
+} else{
+    // Check existence of id parameter
+    if(empty(trim($_GET["id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
 
-        </main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Student Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5 mb-3">Delete Student Record</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="alert alert-danger">
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                            <p>Are you sure you want to delete this Student record?</p>
+                            <p>
+                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <a href="Student.php" class="btn btn-secondary ml-2">No</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>        
+        </div>
     </div>
+</body>
+</html>
 
 <?php
 include('footer.php');
