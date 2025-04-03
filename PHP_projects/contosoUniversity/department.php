65bc4975e63f3ceebaf828d62d7778698d69c09a
Edima <edimaudo@gmail.com>
2025-04-03 16:55:12 -0400

    Update department.php

--- PHP_projects/contosoUniversity/department.php
+++ PHP_projects/contosoUniversity/department.php
@@ -1,34 +1,54 @@
 <?php
 include('header.php');
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+
 ?>
 
 <html>
 <div class="container">
-        <main role="main" class="pb-3">
-           
-<h1>Departments</h1>
-
+<main role="main" class="pb-3">
+<h2>Department</h2>
 <p>
-    <a href="{}">Create New</a>
+    <a href="department_create.php">Create New</a>
 </p>
-<table class="table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Budget</th>
-            <th>Start Date</th>
-            <th>Instructors</th>
-        </tr>
-    </thead>
-    <tbody>
 
-    </tbody>
-</table>
 
-        </main>
-    </div>
-</html>
+<?php
+try {
+
+        $stmt = $pdo->query("SELECT * FROM Department");
+    
+
+    if ($stmt->rowCount() > 0) {
+        echo '<table class="table">';
+        echo "<thead><tr><th>Name</th><th>Budget</th><th>Start Date</th><th>Instructor</th><th></th></tr></thead><tbody>";
+        while ($row = $stmt->fetch()) {
+            echo "<tr>";
+            echo "<td>" . htmlspecialchars($row['Name']) . "</td>";
+            echo "<td>$" . htmlspecialchars($row['Budget']) . "</td>";
+            echo "<td>" . htmlspecialchars( date("Y-m-d",strtotime($row['StartDate']))) . "</td>";
+            echo "<td>" . htmlspecialchars( getInstructorName($pdo,$row['InstructorID'])) . "</td>";
+            echo "<td>";
+            echo '<a href="department_read.php?id=' . $row['DepartmentID'] . '">Details</a> | ';
+            echo '<a href="department_update.php?id=' . $row['DepartmentID'] . '">Edit</a> | ';
+            echo '<a href="department_delete.php?id=' . $row['DepartmentID'] . '">Delete</a>';
+            echo "</td></tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+    }
+} catch (PDOException $e) {
+    echo '<div class="alert alert-danger">Error: ' . htmlspecialchars($e->getMessage()) . '</div>';
+}
+
+unset($pdo);
+?>
 
 <?php
 include('footer.php');
 ?>
+</main>
+</div>
+</html>
