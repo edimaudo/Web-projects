bbf9afe5802ca455092331e9ce395a0e248e9a91
Edima <edimaudo@gmail.com>
2025-04-04 14:57:42 -0400

    Update footer.php

--- PHP_projects/contosoUniversity/footer.php
+++ PHP_projects/contosoUniversity/footer.php
@@ -1,5 +1,5 @@
 <footer class="border-top footer text-muted">
         <div class="container">
-            &copy; 2025 - Contoso University
+            &copy; <?php echo date("Y");?> - Contoso University
         </div>
 </footer>
